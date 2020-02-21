bd9288416d84ac643511cd48c6c6f1587551745f
latheefwac <latheef@webandcrafts.com>
2020-02-21 17:15:16 +0530

    first commit

---
+++ work-flow/assets/js/main.js
@@ -0,0 +1,103 @@
+
+  $('.main-nav ul li:has(ul)').addClass('submenu');
+  $('.main-nav ul li:has(ul)').append("<i></i>");
+  $('.main-nav ul i').click(function() {
+      $(this).parent('li').toggleClass('open');
+      $(this).parent('li').children('ul').slideToggle();
+  })
+
+  //Mobile Menu
+  $('.mob-btn').click(function() {
+      if (!$('html').hasClass('show-menu')) {
+          $('html').addClass('show-menu');
+      } else {
+          $('html').removeClass('show-menu');
+      }
+  });
+
+  $('.overlay').click(function() {
+      if ($('html').hasClass('show-menu')) {
+          $('html').removeClass('show-menu');
+      }
+  });
+  // Slim Header
+  var header = 0;
+  function scrollHead() {
+    if ($(document).scrollTop() > 10) {
+      if (header == 0) {
+        header = 1;
+        $('html').addClass('slim');
+      }
+    } else {
+      if (header == 1) {
+        header = 0;
+        $('html').removeClass('slim');
+      }
+    }
+  }
+  scrollHead();
+  $(window).scroll(scrollHead);
+
+
+  // multiple header
+  // var loc = window.location.href;
+  // var loc_spl = loc.split('/');
+  // if(loc_spl[loc_spl.length-1] == '' || loc_spl[loc_spl.length-1] == 'index.html' || loc_spl[loc_spl.length-1] == 'about-us.html'){
+  //    $('.rz-header').addClass('home-header');
+  // }
+  // else if(loc_spl[loc_spl.length-1] == 'newpage-1.html' || loc_spl[loc_spl.length-1] == 'newpage-2.html' || loc_spl[loc_spl.length-1] == 'newpage-3.html'){
+  //   $('.rz-header').addClass('admin-header');
+  //   $('.rz-header').addClass('inner-header');
+  // }
+  // else{
+  //   $('.rz-header').addClass('inner-header');
+  // }
+// Slick slider
+  //   $('#exp-itemslide').slick({
+  //   draggable: false,
+  //   arrows: false,
+  //   dots: false,
+  //   infinite: false,
+  //   slidesToShow: 6,
+  //   slidesToScroll: 1,	
+  //   responsive: [
+  //     {
+  //         breakpoint: 1450,
+  //         settings: {
+  //           slidesToShow: 5,
+  //           slidesToScroll: 1,
+
+  //         }
+  //       },
+  //       {
+  //         breakpoint: 1199,
+  //         settings: {
+  //           slidesToShow: 3,
+  //           slidesToScroll: 1,
+  //           arrows:true
+  //         }
+  //       },
+  //       {
+  //         breakpoint: 991,
+  //         settings: {
+  //           centerMode: true,
+  //         infinite: true,
+  //         centerPadding: '60px',
+  //         slidesToShow: 2,
+  //         speed: 500,
+  //         variableWidth: false,
+  //         arrows:true
+  //         }
+  //       },{
+  //         breakpoint: 767,
+  //         settings: {
+  //           centerMode: true,
+  //         infinite: true,
+  //         centerPadding: '60px',
+  //         slidesToShow: 1,
+  //         speed: 500,
+  //         variableWidth: false,
+  //         }
+  //       }]
+
+  // });
---
+++ work-flow/index.php
@@ -0,0 +1,7 @@
+<?php include('includes/header.php');?>
+<main class="front-page">
+
+</main>
+<?php include('includes/footer.php');?>
+</body>
+</html>
